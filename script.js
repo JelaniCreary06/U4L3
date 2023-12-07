1d668b9f194ff23ef372db382f102c53b900655f
JelaniSchoolAccount <antwinecreary@gmail.com>
2023-12-07 18:22:33 -0500

    basic chart

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'
 
-let chart = new Chart(document.getElementById("myChart"), {
-  type: "line",
-  data: {
-    labels: ["1", "2", "3", "4", "5"],
-    datasets: [{
-      label: "Series 1",
-      data: [1, 2, 3, 2, 1]
-    },{
-                label: "Series 2",
-                data: [1, 2, 2.5, 2, 1]
-              }]
-  }
-});
-
-
-
 async function wrapper() {
-    let csvData, temp, weatherData;
+    let data = [], temp;
     (await fetch("ZonAnn.Ts+dSST.csv").then(async (response) => {
-        csvData = (await response.text()).split("\n");
-    }))
+        (await response.text()).split("\n").slice(1).forEach((element) => {
+          temp = element.split(",");
+          data.push([temp[0], temp[1]]);
+        })
 
-    console.log(csvData)
+        const chart = new Chart(document.getElementById("chart"), {
+          type: "line",
+          data: {
+            labels: data.map((e) => e[0]),
+            datasets: [{
+              label: `Yearly Temperature ${data[0][0]} - 2022`,
+              data: data.map((e) => e[1]),
+              backgroundColor: '#fc4903'
+            }]
+          }
+        })
+
+        
+    }))
   }
 
   wrapper()
+  
+  localStorage.setItem("t", "p")
