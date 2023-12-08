16cd20d1b3b0ceafa8bd0ca865605d13232b6bab
JelaniSchoolAccount <antwinecreary@gmail.com>
2023-12-07 21:25:10 -0500

    changes

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'
+import 'https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js'
 
 async function wrapper() {
     let data = [], temp;
     (await fetch("ZonAnn.Ts+dSST.csv").then(async (response) => {
         (await response.text()).split("\n").slice(1).forEach((element) => {
           temp = element.split(",");
-          data.push([temp[0], temp[1]]);
+          data.push([temp[0], temp[1], temp[2], temp[3]]);
         })
-
-        const chart = new Chart(document.getElementById("chart"), {
-          type: "line",
-          data: {
-            labels: data.map((e) => e[0]),
-            datasets: [{
-              label: `Yearly Temperature ${data[0][0]} - 2022`,
-              data: data.map((e) => e[1]),
-              backgroundColor: '#fc4903'
-            }]
-          }
-        })
-
-        
     }))
+
+    new Chart(document.getElementById("chart"), {
+      type: "line",
+      data: {
+        labels: data.map((e) => e[0]),
+        datasets: [{
+          label: `Global Temperature`,
+          data: data.map((e) => e[1]),
+          borderColor: '#fc4903',
+          yAxisID: 'y'
+        },
+        {
+          label: `Northern Hemisphere`,
+          data: data.map((e) => e[2]),
+          borderColor: '#0d3370',
+          yAxisID: 'y'
+        },
+        {
+          label: `Southern Hemisphere`,
+          data: data.map((e) => e[3]),
+          borderColor: '#52046e',
+          yAxisID: 'y'
+        }]
+      }, 
+      options: {
+        tooltips: {
+          intersect: true
+        }
+      },
+      scales: {
+        y: {
+          type: 'linear',
+          display: false,
+          position: 'left',
+        },
+        y1: {
+          type: 'linear',
+          display: false,
+          position: 'left',
+        }
+      }
+    })
   }
 
   wrapper()
